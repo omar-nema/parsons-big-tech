3063ba232a7445f55f023b98a5e92e1c0db49689
Omar Nema <omar.nema@arcadiasolutions.com>
2021-03-07 16:02:53 -0500

    okay we gonna package

--- background.js
+++ background.js
@@ -1,35 +1,33 @@
 var status = true;
 
-chrome.runtime.onInstalled.addListener(function(){
+chrome.runtime.onInstalled.addListener(function(e){
 
     chrome.storage.local.set({scrollArray: []});
     chrome.storage.sync.set({pageVisted: false});
 
     //clicking browser action button will change icon, and transmit msg to content script to add remove styles depending on whether enabled or disabled
-    chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.pageAction.onClicked.addListener(function (tab) {
       if (status == 'true'){
         status = false;
-        chrome.browserAction.setIcon({path: "./icon_16px_disable.png"});
+        chrome.pageAction.setIcon({path: "./icon_16px_disable.png", tabId: tab.id});
       } 
-      else{
+      else {
         status = true;
-        chrome.browserAction.setIcon({path: "./icon_16px_enable.png"});
+        chrome.pageAction.setIcon({path: "./icon_16px_enable.png", tabId: tab.id});
       }
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.pageAction.hide(tabs[0].id);
         chrome.tabs.sendMessage(tabs[0].id, {status: status});
       });
-      
-     
   });
 
-  //send extension status on request
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if (request.status == "getStatus")
         sendResponse({status: status});
   });
 
-
+  ///don't need permissions if on instagram.com
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: 
@@ -39,8 +37,7 @@ chrome.runtime.onInstalled.addListener(function(){
           })
           ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
+      }])
     });
-
 
 });
